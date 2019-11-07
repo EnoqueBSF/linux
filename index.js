69392fb4861e4e585fc05d0753bba7108824dc35
Enoque Belmiro <enoquebelmiro01@gmail.com>
2019-11-07 19:56:50 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express"); // Importando o express
+const app = express(); // Iniciando o express
+
+app.get("/",function(req, res){
+    res.send("Bem - vindo ao guia do programador!");
+}); // Criando uma rota
+
+app.get("/blog/:artigo?", function(req, res){
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send(`<h2>Artigo: ${artigo}</h2>`)
+    }else{
+        res.send("Bem - vindo ao meu blog!");
+    }
+})
+
+app.get("/canal/youtube", function(req, res){
+    var canal = req.query["canal"];
+
+    if(canal){
+        res.send(canal);
+    }else{
+        res.send("Nenhum canal fornecido!");
+    }
+})
+
+app.get("/ola/:nome/:empresa", function(req, res){
+    // REQ => DADOS ENVIADOS PELO USUÁRIO
+    // RES => DADOS QUE SERÃO ENVIADOS PARA O USUÁRIO
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send(`<h1>Oi ${nome}, você trabalha na empresa ${empresa}</h1>`);
+})
+
+app.listen(4000, function(erro){
+    if(erro){
+        console.log("Ocorreu um erro!");
+    }else{
+        console.log("Servidor iniciado com sucesso!");
+    }
+});
+
